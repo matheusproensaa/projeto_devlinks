ef39f6af43736b68c0de9473a6976de36cf82423
“matheusproensagit config --global user.email “matheusproensanunes@gmail.com <matheusproensanunes@gmail.comgit config --global user.name matheusproensa>
2023-10-21 03:59:34 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+ 
+  const html = document.documentElement
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+
+  /*
+pode usar 
+htlm.classList.toogle('light')
+*/
+
+  //pegar a imagem
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // se estiver light mode, add imagem light
+
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se estiver sem light, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
